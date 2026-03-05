4e10d671c83bcc82fc8f9ab9d6e2cb70993196b4
Aniket <aniketvaish15@gmail.com>
2026-03-05 19:34:01 +0530

    Fix gdown download link

--- app.py
+++ app.py
@@ -12,13 +12,12 @@ load_dotenv()
 API_KEY = os.getenv("TMDB_API_KEY")
 
 import gdown
-file_id = "1UiS8_1EusgWUmA2WzWnj-o1VApj2AYdl"
-if os.path.exists("similarity.pkl"):
-    os.remove("similarity.pkl")
-    
-if not os.path.exists("similarity.pkl"):
-    url = f"https://drive.google.com/uc?id={file_id}"
-    gdown.download(url, "similarity.pkl", quiet=False, fuzzy=True)
+# file_id = "1UiS8_1EusgWUmA2WzWnj-o1VApj2AYdl"
+# if os.path.exists("similarity.pkl"):
+#     os.remove("similarity.pkl")
+  
+url = f"https://drive.google.com/uc?id={file_id}"
+gdown.download(url, "similarity.pkl", quiet=False, fuzzy=True)
 
 # ---------------- PAGE CONFIG ---------------- #
 st.set_page_config(
